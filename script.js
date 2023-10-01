a252be4b773c62f057fec5b6e1d66066bed69ef0
Ajay Adikari <94204251+ajayadikari@users.noreply.github.com>
2023-10-01 12:19:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let year = date.getFullYear();
 const inputs = document.querySelectorAll('input');
 const loc1 = inputs[0].value || 'delhi';
 const checkIn1 = inputs[1].value || `${year}-${month}-${day}`;
-const checkOut1 = inputs[2].value || `${year}-${month}-${day};
+const checkOut1 = inputs[2].value || `${year}-${month}-${day}`;
 
 console.log(checkIn1, checkOut1);
 const guests1 = inputs[3].value || '2'; 
